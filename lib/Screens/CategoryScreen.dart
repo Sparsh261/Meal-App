adf54eb32bc8414b91e075e2de95f16ba17cdcba
Sparsh261 <sparshchauhan112@gmail.com>
2025-06-04 15:02:55 +0530

    first commit

---
+++ lib/Screens/CategoryScreen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class  CategoryScreen extends StatelessWidget {
+
+@override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Pick category"),),
+      body: Column(
+        children: [
+          
+        ],
+      ),
+    )
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:mymealapp/Screens/CategoryScreen.dart';
+
+void main() {
+  runApp(MaterialApp(home: CategoryScreen(),));
+}
